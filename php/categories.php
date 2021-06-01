fd12f1d1ce0fe05e72e76d366e393076a91f1511
xChoki <javier.quinmal@hotmail.cl>
2021-06-01 18:45:50 -0400

    php de Lugares Recomendados añadido, no se logra solucionar el error encontrado

---
+++ php/categories.php
@@ -0,0 +1,15 @@
+{"categories":[{
+            "idCategory":"1",
+            "strCategory":"Govindas Restaurant Vegetariano",
+            "strCategoryThumb":"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d26632.88067435503!2d-70.67603255333178!3d-33.446438373797676!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x418cb52479f3cebf!2sGovindas%20Restaurant%20Vegetariano!5e0!3m2!1ses!2scl!4v1622585290091!5m2!1ses!2scl",
+            "strCategoryDescription":"Buen lugar de comida vegetariana ubicado en Av. Ricardo Cumming 643, Santiago, Región Metropolitana. Tiene comida para llevar."
+        },
+
+        {
+            "idCategory":"2",
+            "strCategory":"Chicken",
+            "strCategoryThumb":"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d26632.88067435503!2d-70.67603255333178!3d-33.446438373797676!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x418cb52479f3cebf!2sGovindas%20Restaurant%20Vegetariano!5e0!3m2!1ses!2scl!4v1622585290091!5m2!1ses!2scl",
+            "strCategoryDescription":"Chicken is a type of domesticated fowl, a subspecies of the red junglefowl. It is one of the most common and widespread domestic animals, with a total population of more than 19 billion as of 2011.[1] Humans commonly keep chickens as a source of food (consuming both their meat and eggs) and, more rarely, as pets."
+        }
+    ]   
+}
